186195c83e11f86968b1da7a222e5cb94226668e
hostfreecloud <127306848+hostfreecloud@users.noreply.github.com>
2023-11-23 23:15:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/hostfreecloud/Zoro-Filter-BOT.git /Zoro-Filter-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Zoro-Filter-BOT
 fi
-cd /VJ-FILTER-BOT 
+cd /Zoro-Filter-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
